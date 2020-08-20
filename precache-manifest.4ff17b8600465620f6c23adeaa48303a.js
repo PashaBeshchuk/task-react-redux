7ff8c78184fb7bd0cb1a5b7671ff154e77df4c31
Pasha Beshchuk <pashabeshchuk1990@gmail.com>
2020-08-20 21:35:31 +0300

    Updates

---
+++ precache-manifest.4ff17b8600465620f6c23adeaa48303a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7f7aa8d339d7e3e9ac2e911add06cb",
+    "url": "/task-react-redux/index.html"
+  },
+  {
+    "revision": "78b8f58427b3fafdbf54",
+    "url": "/task-react-redux/static/css/2.1b92d8a2.chunk.css"
+  },
+  {
+    "revision": "7b012912706e0d0c6dd8",
+    "url": "/task-react-redux/static/css/main.bda3f0e1.chunk.css"
+  },
+  {
+    "revision": "78b8f58427b3fafdbf54",
+    "url": "/task-react-redux/static/js/2.7d414e76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-react-redux/static/js/2.7d414e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b012912706e0d0c6dd8",
+    "url": "/task-react-redux/static/js/main.fafea836.chunk.js"
+  },
+  {
+    "revision": "4079920058ac528d9104",
+    "url": "/task-react-redux/static/js/runtime-main.fb94b09d.js"
+  }
+]);
